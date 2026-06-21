ec1eff8591e072fd9a666861cff800f23bff5bd9
Sina Bolouki <sina.bolouki@gmail.com>
2026-06-21 19:51:59 +0200

    update config

--- flask_app/config_example.py
+++ flask_app/config_example.py
@@ -1,12 +1,12 @@
 import os
 STIMULI_UPLOAD_PATH = 'static/artworks/'
 DB_TYPE = 'postgresql+psycopg2'
-DB_NAME = 'livegaze'
-DB_HOST = 'localhost'
-DB_USER = 'testuser'
-DB_PORT = 5432
-DB_PASSWORD = 'testPassword'
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL",DB_TYPE+"://"+DB_USER+":"+DB_PASSWORD+"@"+DB_HOST+":"+str(DB_PORT)+"/"+DB_NAME)
+POSTGRES_DB = os.getenv('POSTGRES_DB','livegaze')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST','localhost')
+POSTGRES_USER = os.getenv('POSTGRES_USER','testuser')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT',5432)
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD','testPassword')
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL",DB_TYPE+"://"+POSTGRES_USER+":"+POSTGRES_PASSWORD+"@"+POSTGRES_HOST+":"+str(POSTGRES_PORT)+"/"+POSTGRES_DB)
 SECRET_KEY="powerful secretkey"
 WTF_CSRF_SECRET_KEY="a csrf secret key"
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
